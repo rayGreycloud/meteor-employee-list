3d1b2ce1ca54c18fcd8cdfab4dba932bba06983c
rayGreycloud <rayGreycloud@gmail.com>
2016-08-05 14:34:13 -0700

    Add button to show more cards

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -3,6 +3,8 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
 import EmployeeDetail from './employee_detail';
 
+const PER_PAGE = 20;
+
 const EmployeeList = (props) => {
 
   return (
@@ -11,13 +13,16 @@ const EmployeeList = (props) => {
         {props.employees.map(employee =>
           <EmployeeDetail key={employee._id} employee={employee} />)}
       </div>
+      <button onClick={() => Meteor.subscribe('employees', 40) }
+      className="btn btn-primary">Load More...
+      </button>
     </div>
   );
 };
 
 export default createContainer(() => {
   // set up subscription
-  Meteor.subscribe('employees');
+  Meteor.subscribe('employees', PER_PAGE);
 
   // return object to EmployeeList as props
   return { employees: Employees.find({}).fetch() };
